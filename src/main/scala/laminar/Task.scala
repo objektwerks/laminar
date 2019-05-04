61313b45b2999cde430318ed638b231e04a0c743
objektwerks <objektwerks@runbox.com>
2019-05-04 12:01:13 -0400

    edited Task

--- src/main/scala/laminar/Task.scala
+++ src/main/scala/laminar/Task.scala
@@ -4,22 +4,22 @@ import java.util.concurrent.atomic.AtomicInteger
 
 import com.raquo.laminar.api.L._
 
-class Task private(val id: Int, val task: String, val element: HtmlElement)
+class Task private(val id: Int, val item: String, val element: HtmlElement)
 
 object Task {
   val id = new AtomicInteger()
 
   def newId:Int = id.incrementAndGet
 
-  def apply(id: Int = newId, task: String, element: HtmlElement): String = {
+  def apply(id: Int = newId, task: String, element: HtmlElement): Task = {
     val taskStream: EventStream[List[Task]] = EventStream(List.empty[Task])
 
     def render(id: Int, task: String, taskStream: EventStream[Task]): Div = {
       div(
-        p("task: ", child.text <-- taskStream.map(_.task))
+        p("task: ", child.text <-- taskStream.map(_.item))
       )
     }
-    val element = div("Tasks: ", children <-- taskStream.split(_.id)(render(_, task, taskStream)))
+    val element = div(children <-- taskStream.split(_.id)(render(_, task, taskStream)))
     new Task(id, task, element)
   }
 }
