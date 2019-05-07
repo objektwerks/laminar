ff31b93e1fe24eb5334ad1d48da8a2ec8e507929
objektwerks <objektwerks@runbox.com>
2019-05-07 07:31:23 -0400

    added input box

---
+++ src/main/scala/laminar/InputBox.scala
@@ -0,0 +1,13 @@
+package laminar
+
+import com.raquo.laminar.api.L._
+
+class InputBox private (val element: HtmlElement, val box: Input)
+
+object InputBox {
+  def apply(label: String, typeof: String): InputBox = {
+    val box = input(typ := typeof)
+    val element = div(label, box)
+    new InputBox(element, box)
+  }
+}
