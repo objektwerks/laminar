fecc3a309764219e282ddbc8b09a1f869c1d85a2
objektwerks <objektwerks@runbox.com>
2019-05-07 10:53:49 -0400

    refactored input box class args

--- src/main/scala/laminar/InputBox.scala
+++ src/main/scala/laminar/InputBox.scala
@@ -2,12 +2,12 @@ package laminar
 
 import com.raquo.laminar.api.L._
 
-class InputBox private(val element: HtmlElement, val box: Input)
+class InputBox private(val element: HtmlElement, val eventSource: Input)
 
 object InputBox {
   def apply(label: String, typeof: String): InputBox = {
-    val box = input(typ := typeof)
-    val element = div(label, box)
-    new InputBox(element, box)
+    val eventSource = input(typ := typeof)
+    val element = div(label, eventSource)
+    new InputBox(element, eventSource)
   }
 }
