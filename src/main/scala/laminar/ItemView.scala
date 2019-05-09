1663733e5006f1c1781f1b4709f848d8776d3125
objektwerks <objektwerks@runbox.com>
2019-05-09 19:05:10 -0400

    added list delete button

--- src/main/scala/laminar/ItemView.scala
+++ src/main/scala/laminar/ItemView.scala
@@ -5,6 +5,8 @@ import com.raquo.laminar.api.L._
 class ItemView private(val element: HtmlElement)
 
 object ItemView {
+  import UnsafeInnerHtmlModifier._
+
   def apply(items: List[Item], header: String): ItemView = {
     val itemsStream: EventStream[List[Item]] = EventStream.fromValue(items, true)
     val lisStream: EventStream[List[Li]] = itemsStream.map(items => items.map(renderItem))
@@ -12,7 +14,17 @@ object ItemView {
     new ItemView(div)
   }
 
-  def renderItem(item: Item): Li = li(item.render)
+  def renderItem(item: Item): Li = li(
+    cls := "w3-display-container",
+    item.render,
+    inContext { liNode =>
+      span(
+        cls := "w3-button w3-display-right",
+        onClick --> { _: Any => display.none(liNode) },
+        unsafeInnerHtml := "&times;"
+      )
+    }
+  )
 
   def renderItems(elements: EventStream[List[Li]], header: String): Div = {
     div(
