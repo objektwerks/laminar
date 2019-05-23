478a7ad51aa615def07fdf761589ee519ce349ae
objektwerks <objektwerks@runbox.com>
2019-05-23 18:23:17 -0400

    Tasks now mimics Items - with refactoring to table next

--- src/main/scala/laminar/Tasks.scala
+++ src/main/scala/laminar/Tasks.scala
@@ -2,8 +2,11 @@ package laminar
 
 import java.time.Instant
 
+import com.raquo.airstream.signal.{Signal, Var}
+import com.raquo.laminar.api.L._
+
 case class Task(id: String = Task.newId(),
-                task: String,
+                value: String,
                 opened: Long = Instant.now.toEpochMilli,
                 closed: Long = Instant.now.toEpochMilli)
 
@@ -14,5 +17,107 @@ object Task {
 }
 
 object Tasks {
+  def apply(tasksVar: Var[List[Task]]): HtmlElement = new View(new Model(tasksVar)).render
+
+  private class Logger(tasksVar: Var[List[Task]]) {
+    import com.raquo.airstream.ownership.Owner
+    import org.scalajs.dom.console._
+
+    implicit val owner = new Owner {}
+    tasksVar.signal.foreach(tasks => log(s"tasks change event -> ${tasks.toString}"))
+  }
+
+  private class Model(val tasksVar: Var[List[Task]]) {
+    val _ = new Logger(tasksVar)
+    val selectedItemVar: Var[Option[Task]] = Var(None)
+
+    def selectedItem: Task = selectedItemVar.now.getOrElse(Task.empty)
+
+    def onSelectItem(id: String): Unit = {
+      val task = tasksVar.now.find(_.id == id)
+      selectedItemVar.set(task)
+    }
+
+    def onAddItem(value: String): Unit = tasksVar.update(_ :+ Task(value = value))
+
+    def onEditItem(id: String, value: String): Unit = {
+      tasksVar.update(_.map(task => if (task.id == id) task.copy(value = value) else task))
+      selectedItemVar.set(None)
+    }
+
+    def onRemoveItem(id: String): Unit = tasksVar.update(_.filterNot(_.id == id))
+  }
+
+  private class View(model: Model) {
+    import InnerHtmlModifier._
+    import org.scalajs.dom.ext.KeyCode
+
+    val onEnterPress = onKeyPress.filter(_.keyCode == KeyCode.Enter)
+
+    def render: HtmlElement = renderRoot(model.tasksVar.signal.split(_.id)((_, task, taskSignal) => renderTask(task, taskSignal)))
+
+    def renderRoot(tasksSignal: Signal[List[Li]]): HtmlElement =
+      div(cls("w3-container"),
+        div(
+          h4(cls("w3-light-grey w3-text-indigo"), "Task"),
+          renderAddTask,
+          renderEditTask
+        ),
+        div(
+          h4(cls("w3-light-grey w3-text-indigo"), "Items"),
+          renderTasks(tasksSignal)
+        )
+      )
+
+    def renderTask(task: Task, taskSignal: Signal[Task]): Li =
+      li(cls("w3-text-indigo w3-display-container"),
+        child.text <-- taskSignal.map(task.id + ". " + _.value),
+        span(cls("w3-button w3-display-right w3-text-indigo"),
+          onClick.mapTo(task.id) --> { id => model.onRemoveItem(id) },
+          unsafeInnerHtml := "&times;"
+        ),
+        onClick.mapTo(task.id) --> { id => model.onSelectItem(id) }
+      )
+
+    def renderTasks(tasksSignal: Signal[List[Li]]): Div =
+      div(cls("w3-container"),
+        ul(cls("w3-ul w3-hoverable"), children <-- tasksSignal)
+      )
+
+    def renderAddTask: Div =
+      div(cls("w3-container"), paddingTop("3px"), paddingBottom("3px"),
+        div(cls("w3-row"),
+          div(cls("w3-col"), width("15%"), label(cls("w3-left-align w3-text-indigo"), "Add:")),
+          div(cls("w3-col"), width("85%"),
+            input(cls("w3-input w3-hover-light-gray w3-text-indigo"), typ("text"),
+              inContext { input =>
+                onEnterPress.mapTo(input.ref.value).filter(_.nonEmpty) --> { value =>
+                  model.onAddItem(value)
+                  input.ref.value = ""
+                }
+              }
+            )
+          )
+        )
+      )
+
+    def renderEditTask: Div =
+      div(cls("w3-container"), paddingTop("3px"), paddingBottom("3px"),
+        div(cls("w3-row"),
+          div(cls("w3-col"), width("15%"), label(cls("w3-left-align w3-text-indigo"), "Edit:")),
+          div(cls("w3-col"), width("85%"),
+            input(cls("w3-input w3-hover-light-gray w3-text-indigo"), typ("text"), readOnly(true),
+              value <-- model.selectedItemVar.signal.map(_.getOrElse(Task.empty).value),
+              readOnly <-- model.selectedItemVar.signal.map(_.isEmpty),
+              inContext { input =>
+                onEnterPress.mapTo(input.ref.value).filter(_.nonEmpty) --> { value =>
+                  model.onEditItem(model.selectedItem.id, value)
+                }
+              }
+            )
+          )
+        )
+      )
+  }
 
 }
