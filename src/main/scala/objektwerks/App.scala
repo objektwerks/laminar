9a8db6ef991823d03c62279a08264e19d0e3d19a
objektwerks <objektwerks@runbox.com>
2020-04-07 08:29:25 -0400

    refactored App with createItems()

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -10,9 +10,13 @@ object App {
   @JSExport
   def init(): Unit = {
     val app = document.getElementById("app")
-    val itemsVar = Var(List[Item](Item(value = "wash car"), Item(value = "mow yard"), Item(value = "clean pool")))
-    val items = Items(itemsVar)
+    val items = createItems()
     render(app, items)
     ()
+  }
+
+  private def createItems(): HtmlElement = {
+    val itemsVar = Var(List[Item](Item(value = "wash car"), Item(value = "mow yard"), Item(value = "clean pool")))
+    Items(itemsVar)
   }
 }
