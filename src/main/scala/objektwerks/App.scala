e1bf9b15478d50d269217a9e990b837dfbc9d3ed
objektwerks <objektwerks@runbox.com>
2020-04-07 15:39:46 -0400

    added tasks to App

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -11,12 +11,18 @@ object App {
   def init(): Unit = {
     val app = document.getElementById("app")
     val items = createItems()
-    render(app, items)
+    val tasks = createTasks()
+    render(app, div(items, tasks))
     ()
   }
 
   private def createItems(): HtmlElement = {
     val itemsVar = Var(List[Item](Item(value = "wash car"), Item(value = "mow yard"), Item(value = "clean pool")))
     Items(itemsVar)
+  }
+
+  private def createTasks(): HtmlElement = {
+    val tasksVar = Var(List[Task](Task(value = "clean kitchen"), Task(value = "polish silver"), Task(value = "clean floors")))
+    Tasks(tasksVar)
   }
 }
