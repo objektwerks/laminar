b851c33415a569d469ba0f8502e83eed928b6d74
objektwerks <objektwerks@runbox.com>
2021-03-22 18:27:32 -0400

    moved Id to Items for clarity

--- src/main/scala/objektwerks/Items.scala
+++ src/main/scala/objektwerks/Items.scala
@@ -1,5 +1,17 @@
 package objektwerks
 
+class Id() {
+  private var autoinc = 0
+  def increment(): String = {
+    autoinc = autoinc + 1
+    autoinc.toString
+  }
+}
+
+object Id {
+  def apply(): Id = new Id()
+}
+
 case class Item(id: String = Item.id.increment(), value: String)
 
 object Item {
