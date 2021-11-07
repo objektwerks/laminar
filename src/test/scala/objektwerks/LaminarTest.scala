5cbb83c34a5b493aa7d4727c5a57f9bca0c65436
objektwerks <objektwerks@runbox.com>
2021-11-07 07:48:47 -0500

    add test to laminar test

--- src/test/scala/objektwerks/LaminarTest.scala
+++ src/test/scala/objektwerks/LaminarTest.scala
@@ -1,11 +1,14 @@
 package objektwerks
 
+import com.raquo.laminar.api.L._
+
 import utest._
 
 object LaminarTest extends TestSuite {
   val tests = Tests {
-    test("laminar") {
-      // TODO
+    test("var") {
+      val integerVar = Var(3)
+      assert( integerVar.now() == 3 )
     }
   }
 }
