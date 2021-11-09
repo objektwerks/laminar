24a928601d804b5e8c2b012d8571b09b2d727116
objektwerks <objektwerks@runbox.com>
2021-11-09 07:15:27 -0500

    enhanced signal test

--- src/test/scala/objektwerks/LaminarTest.scala
+++ src/test/scala/objektwerks/LaminarTest.scala
@@ -23,14 +23,14 @@ object LaminarTest extends TestSuite {
     test("eventbus") {
       val intEventBus = new EventBus[Int]()
       implicit val owner = new Owner {}
-      intEventBus.events.foreach { i => assert(i == 1) }
+      intEventBus.events.foreach { i => assert( i == 1 ) }
       intEventBus.emit(1)
     }
 
     test("eventstream") {
       val (stream, callback) = EventStream.withCallback[Int]
       implicit val owner = new Owner {}
-      stream.foreach { i => assert(i == 1) }
+      stream.foreach { i => assert( i == 1 ) }
       callback(1)
     }
 
@@ -38,8 +38,10 @@ object LaminarTest extends TestSuite {
       val (stream, callback) = EventStream.withCallback[Int]
       val intSignal = stream.startWith(0)
       implicit val owner = new Owner {}
-      intSignal.changes.foreach { i => assert(i == 1) }
+      intSignal.changes.foreach { i => assert( i == 1 ) }
+      assert( intSignal.observe.now() == 0 )
       callback(1)
+      assert( intSignal.observe.now() == 1 )
     }
   }
 }
