1b82108d8077a4002bbdec9b0404ae291515712b
objektwerks <objektwerks@runbox.com>
2021-11-09 09:32:12 -0500

    added async laminar test

---
+++ src/test/scala/objektwerks/LaminarAsyncTest.scala
@@ -0,0 +1,28 @@
+package objektwerks
+
+import com.raquo.airstream.web.AjaxEventStream
+import com.raquo.airstream.web.AjaxEventStream.AjaxStreamError
+import com.raquo.laminar.api.L._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import utest._
+
+object LaminarAsyncTest extends TestSuite {
+  val tests = Tests {
+    test("ajax") {
+      val stream = AjaxEventStream
+        .get("http://api.icndb.com/jokes/random/")
+        .map { response => response.responseText
+        }
+        .recover { case error: AjaxStreamError => Some(error.getMessage) }
+      val signal = stream.toSignal("").observe(new Owner {})
+      signal.now()
+    }
+  }
+  TestRunner.runAsync(tests).map { asyncResults =>
+    val results = asyncResults.leaves.toSeq
+    assert(results.head.value.isSuccess)
+    assert(results.tail.head.value.isSuccess)
+  }
+}
