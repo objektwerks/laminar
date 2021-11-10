22365e117fe98b34cb7fb63df6c06523bd3b1234
objektwerks <objektwerks@runbox.com>
2021-11-10 05:17:48 -0500

    added json parse method to airstream async test

--- src/test/scala/objektwerks/AirstreamAsyncTest.scala
+++ src/test/scala/objektwerks/AirstreamAsyncTest.scala
@@ -9,19 +9,26 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import utest._
 
 object AirstreamAsyncTest extends TestSuite {
+  def parse(joke: String): String = {
+    val json = ujson.read(joke)
+    json("value").obj("joke").str
+  }
+
   val tests = Tests {
     test("ajax") {
       val stream = AjaxEventStream
         .get("http://api.icndb.com/jokes/random/")
         .map { response =>
-          println(response.responseText)
-          response.responseText
+          val joke = parse(response.responseText)
+          println(s"*** $joke")
+          joke
         }
         .recover { case error: AjaxStreamError => Some(error.getMessage) }
       val signal = stream.toSignal("").observe(new Owner {})
       signal.now()
     }
   }
+
   TestRunner.runAsync(tests).map { asyncResults =>
     val results = asyncResults.leaves.toSeq
     assert(results.head.value.isSuccess)
