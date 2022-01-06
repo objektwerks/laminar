abc5191b9ec63b4369bd73000d703e13d7fdd1ad
objektwerks <objektwerks@runbox.com>
2022-01-06 09:11:26 -0500

    reformatted dates

--- src/main/scala/objektwerks/Dates.scala
+++ src/main/scala/objektwerks/Dates.scala
@@ -4,20 +4,17 @@ import org.scalajs.dom.console._
 
 import scala.scalajs.js.Date
 
-object Dates {
+object Dates:
   def nowAsString(): String = toDateAsString(Date.now())
 
-  def toDateAsString(timestamp: Double): String = {
+  def toDateAsString(timestamp: Double): String =
     val iso = new Date(timestamp).toISOString()
     val isoDate = iso.substring(0, iso.lastIndexOf("T"))
     log("iso: ", iso)
     log("iso date: ", isoDate)
     isoDate
-  }
 
-  def toDateAsDouble(date: String): Double = {
+  def toDateAsDouble(date: String): Double =
     val isoDate = new Date(date).valueOf()
     log("iso date: ", isoDate)
     isoDate
-  }
-}
