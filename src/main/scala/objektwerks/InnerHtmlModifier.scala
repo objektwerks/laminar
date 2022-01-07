eae6e1b5125da507b375a0d358d1f080de4e5467
objektwerks <objektwerks@runbox.com>
2022-01-06 19:50:11 -0500

    edited import

--- src/main/scala/objektwerks/InnerHtmlModifier.scala
+++ src/main/scala/objektwerks/InnerHtmlModifier.scala
@@ -1,6 +1,6 @@
 package objektwerks
 
-import com.raquo.laminar.api.L._
+import com.raquo.laminar.api.L.*
 
 object InnerHtmlModifier:
   val unsafeInnerHtml: InnerHtmlModifier.type = InnerHtmlModifier
