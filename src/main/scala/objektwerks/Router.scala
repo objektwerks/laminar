2a97db89516128662b7bf60e403f0741633696f9
objektwerks <objektwerks@runbox.com>
2022-01-07 09:10:19 -0500

    implemented router

--- src/main/scala/objektwerks/Router.scala
+++ src/main/scala/objektwerks/Router.scala
@@ -1,3 +1,29 @@
 package objektwerks
 
-sealed trait Router
+import com.raquo.laminar.api.L
+import com.raquo.waypoint.*
+
+import upickle.default.*
+
+import org.scalajs.dom
+
+import Serializer.given
+
+sealed trait Router:
+  val loginRoute = Route.static(LoginPage, root / "login" / endOfSegments)
+  
+  val itemsRoute = Route[ItemsPage, List[Item]](
+    encode = itemsPage => itemsPage.items,
+    decode = items => ItemsPage(items),
+    pattern = root / "items" / segment[List[Item]] / endOfSegments
+  )
+
+  val router = new com.raquo.waypoint.Router[Page](
+    routes = List(loginRoute, itemsRoute),
+    getPageTitle = _.toString,
+    serializePage = page => write(page)(pageRW),
+    deserializePage = pageStr => read(pageStr)(pageRW)
+  )(
+    $popStateEvent = L.windowEvents.onPopState,
+    owner = L.unsafeWindowOwner
+  )
