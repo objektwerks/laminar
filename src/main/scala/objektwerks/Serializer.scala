c7b228ff5d2159b4ce81c357520369cc5d0fe583
objektwerks <objektwerks@runbox.com>
2022-01-07 11:50:33 -0500

    refactored serializers

--- src/main/scala/objektwerks/Serializer.scala
+++ src/main/scala/objektwerks/Serializer.scala
@@ -4,5 +4,4 @@ object Serializer:
   import upickle.default.*
 
   given itemRW: ReadWriter[Item] = macroRW
-  given itemsPageRW: ReadWriter[ItemsPage] = macroRW
-  given pageRW: ReadWriter[Page] = ReadWriter.merge( itemsPageRW )
+  given pageRW: ReadWriter[Page] = macroRW
