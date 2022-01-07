735a679e1af65793b40b6cfcaf2ff41c41930ebf
objektwerks <objektwerks@runbox.com>
2022-01-07 03:08:44 -0500

    refactored items page

--- src/main/scala/objektwerks/Page.scala
+++ src/main/scala/objektwerks/Page.scala
@@ -4,4 +4,4 @@ import com.raquo.laminar.api.L.*
 
 sealed trait Page
 case object LoginPage extends Page
-final case class ItemsPage(itemsVar: Var[List[Item]]) extends Page
+final case class ItemsPage(items: List[Item]) extends Page
