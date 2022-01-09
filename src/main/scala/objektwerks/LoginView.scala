f87979b881e660c87ceef2c294605d1d897528b5
objektwerks <objektwerks@runbox.com>
2022-01-09 09:33:43 -0500

    added pin label and input to login view

--- src/main/scala/objektwerks/LoginView.scala
+++ src/main/scala/objektwerks/LoginView.scala
@@ -12,6 +12,14 @@ object LoginView:
         typ("email"),
         required(true)
       ),
+      label(cls("w3-left-align w3-text-indigo"), "Pin"),
+      input(
+        cls("w3-input w3-hover-light-gray w3-text-indigo"),
+        typ("number"),
+        pattern("\\d*"),
+        stepAttr("1"),
+        required(true)
+      )
       button(cls("w3-bar-item w3-button w3-text-indigo"), "Login").amend {
         onClick --> { _ =>
           Router.router.pushState(ItemsPage)
