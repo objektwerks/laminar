30ae367938d57cb94d1c4a50f4dc16b7b5f01feb
objektwerks <objektwerks@runbox.com>
2022-01-08 20:26:33 -0500

    added label to login view

--- src/main/scala/objektwerks/LoginView.scala
+++ src/main/scala/objektwerks/LoginView.scala
@@ -6,6 +6,7 @@ object LoginView:
   def apply(): HtmlElement =
     div(
       h4(cls("w3-light-grey w3-text-indigo"), "Login"),
+      label(cls("w3-left-align w3-text-indigo"), "Email Address"),
       input(
         cls("w3-input w3-hover-light-gray w3-text-indigo"),
         typ("text"),
