96d49d221aea950e7bb8c134c50216bbd193c9c3
objektwerks <objektwerks@runbox.com>
2022-01-10 04:01:53 -0500

    implemented index view

--- src/main/scala/objektwerks/IndexView.scala
+++ src/main/scala/objektwerks/IndexView.scala
@@ -2,7 +2,17 @@ package objektwerks
 
 import com.raquo.laminar.api.L.*
 
-object IndexView {
+import org.scalajs.dom.console.log
+
+object IndexView:
     def apply(): HtmlElement =
-      div()
-}
+      div(cls("w3-container"),
+        div(cls("w3-bar w3-white w3-text-indigo"),
+          a(href("#"), cls("w3-bar-item w3-button"), "Login").amend {
+            onClick --> { _ =>
+              log("Login onClick")
+              Router.router.pushState(LoginPage)
+            }
+          }
+        )
+      )
