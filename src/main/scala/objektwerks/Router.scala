bca9e8781da6a1e0bbd7f800340aecffb419819c
objektwerks <objektwerks@runbox.com>
2022-01-10 03:51:25 -0500

    added index page to router

--- src/main/scala/objektwerks/Router.scala
+++ src/main/scala/objektwerks/Router.scala
@@ -11,11 +11,12 @@ import org.scalajs.dom
 import Serializer.given
 
 object Router:
-  private val loginPageRoute = Route.static(LoginPage, root / endOfSegments)
+  private val indexPageRoute = Route.static(IndexPage, root / endOfSegments)
+  private val loginPageRoute = Route.static(LoginPage, root / "login" / endOfSegments)
   private val itemsPageRoute = Route.static(ItemsPage, root / "items" / endOfSegments)
 
   val router = new com.raquo.waypoint.Router[Page](
-    routes = List(loginPageRoute, itemsPageRoute),
+    routes = List(indexPageRoute, loginPageRoute, itemsPageRoute),
     serializePage = page => write(page)(pageRW),
     deserializePage = pageAsString => read(pageAsString)(pageRW),
     getPageTitle = _.toString,
@@ -25,5 +26,6 @@ object Router:
   )
 
   val splitter = SplitRender[Page, HtmlElement](router.$currentPage)
+    .collectStatic(IndexPage) { LoginView() }
     .collectStatic(LoginPage) { LoginView() }
     .collectStatic(ItemsPage) { ItemsView() }
