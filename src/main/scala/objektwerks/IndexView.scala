af744d888f5e636734639a0f44252c3711757170
objektwerks <objektwerks@runbox.com>
2022-01-10 09:47:15 -0500

    edited index.html and index view

--- src/main/scala/objektwerks/IndexView.scala
+++ src/main/scala/objektwerks/IndexView.scala
@@ -6,8 +6,8 @@ import org.scalajs.dom.console.log
 
 object IndexView:
     def apply(): HtmlElement =
-      div(cls("w3-container"),
-        div(cls("w3-bar w3-white w3-text-indigo"),
+      div(
+        div(cls("w3-bar w3-light-grey w3-text-indigo"),
           a(href("#"), cls("w3-bar-item w3-button"), "Login").amend {
             onClick --> { _ =>
               log("Login onClick")
@@ -15,9 +15,9 @@ object IndexView:
             }
           }
         ),
-        div(cls("w3-container"),
-          h4(cls("w3-light-grey w3-text-indigo"), "Features"),
-          ul(cls("w3-ul w3-hoverable"),
+        div(
+          h5(cls("w3-light-grey w3-text-indigo"), "Features"),
+          ul(cls("w3-ul"),
             li(cls("w3-text-indigo w3-display-container"), "Add Task"),
             li(cls("w3-text-indigo w3-display-container"), "Edit Task"),
             li(cls("w3-text-indigo w3-display-container"), "List Tasks")
