7cf73a50b815e5c25f29256f56bc0c4f3547ef22
objektwerks <objektwerks@runbox.com>
2022-01-11 03:46:16 -0500

    added title to page

--- src/main/scala/objektwerks/Page.scala
+++ src/main/scala/objektwerks/Page.scala
@@ -2,7 +2,8 @@ package objektwerks
 
 import com.raquo.laminar.api.L.*
 
-sealed trait Page
+sealed trait Page:
+  val title = "Tasks"
 case object IndexPage extends Page
 case object LoginPage extends Page
 case object ItemsPage extends Page
