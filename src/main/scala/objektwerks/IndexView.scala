7892330b573a75b5044d1cbe7266b02fc870bf57
objektwerks <objektwerks@runbox.com>
2022-01-12 11:48:42 -0500

    restyled header element in index view feature list

--- src/main/scala/objektwerks/IndexView.scala
+++ src/main/scala/objektwerks/IndexView.scala
@@ -8,7 +8,7 @@ object IndexView:
     def apply(): HtmlElement =
       div(
         div(cls("w3-bar w3-text-indigo"),
-          button(cls("w3-button  w3-round-xxlarge w3-light-grey w3-text-indigo"), "Login").amend {
+          button(cls("w3-button w3-round-xxlarge w3-light-grey w3-text-indigo"), "Login").amend {
             onClick --> { _ =>
               log("Login onClick")
               Router.router.pushState(LoginPage)
@@ -17,7 +17,7 @@ object IndexView:
         ),
         div(
           ul(cls("w3-ul"),
-            li(h5(cls("w3-text-indigo"), "Features:")),
+            li(h5(cls("w3-light-grey w3-text-indigo"), "Features:")),
             li(cls("w3-text-indigo"), "Add Task"),
             li(cls("w3-text-indigo"), "Edit Task"),
             li(cls("w3-text-indigo"), "List Tasks")
