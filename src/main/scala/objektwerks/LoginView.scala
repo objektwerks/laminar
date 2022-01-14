3ed33bcfca08b2b720a7624394ebf67697e48c67
objektwerks <objektwerks@runbox.com>
2022-01-14 02:42:14 -0500

    removed incontext for email and pin; added controlled

--- src/main/scala/objektwerks/LoginView.scala
+++ src/main/scala/objektwerks/LoginView.scala
@@ -14,10 +14,10 @@ object LoginView:
         typ("email"),
         minLength(3),
         required(true),
-        value <-- emailAddress,
-        inContext { input =>
+        controlled(
+          value <-- emailAddress,
           onInput.mapToValue.filter(_.nonEmpty) --> emailAddress
-        }
+        )
       ),
       label(cls("w3-left-align w3-text-indigo"), "Pin"),
       input(
@@ -26,10 +26,10 @@ object LoginView:
         minLength(6),
         maxLength(6),
         required(true),
-        value <-- pin,
-        inContext { input =>
+        controlled(
+          value <-- pin,
           onInput.mapToValue.filter(_.nonEmpty) --> pin
-        }
+        )
       ),
       div(cls("w3-bar w3-margin-top w3-center"),
         button(cls("w3-button w3-round-xxlarge w3-light-gray w3-text-indigo"), "Login").amend {
