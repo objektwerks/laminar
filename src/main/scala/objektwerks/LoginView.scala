a9860486043e8dc21013ee736347fa599d85c26e
objektwerks <objektwerks@runbox.com>
2022-01-14 08:10:37 -0500

    implemented error reporting in login view

--- src/main/scala/objektwerks/LoginView.scala
+++ src/main/scala/objektwerks/LoginView.scala
@@ -8,6 +8,8 @@ import Validator.*
 
 object LoginView:
   def apply(emailAddress: Var[String], pin: Var[String]): HtmlElement =
+    val emailAddressError = new EventBus[String]
+    val pinError = new EventBus[String]
     div(cls("w3-container"),
       h5(cls("w3-light-grey w3-text-indigo"), "Login"),
       label(cls("w3-left-align w3-text-indigo"), "Email Address"),
@@ -20,9 +22,11 @@ object LoginView:
         value <-- emailAddress,
         onInput.mapToValue.filter(_.nonEmpty).setAsValue --> emailAddress,
         onKeyUp.mapToValue --> { value =>
+          if value.isEmailAddress() then emailAddressError.emit("")
+          else emailAddressError.emit("Please, enter a valid email address.")
         }
       ),
-      label(idAttr("email-address-error"), cls("w3-left-align w3-text-red")),
+      div(cls("w3-border-white w3-margin w3-red"), child.text <-- emailAddressError.events),
       label(cls("w3-left-align w3-text-indigo"), "Pin"),
       input(
         cls("w3-input w3-hover-light-gray w3-text-indigo"),
@@ -34,8 +38,11 @@ object LoginView:
         value <-- pin,
         onInput.mapToValue.filter(_.nonEmpty).setAsValue --> pin,
         onKeyUp.mapToValue --> { value =>
-        }      ),
-      label(idAttr("pin-error"), cls("w3-left-align w3-text-red")),
+          if value.isPin() then pinError.emit("")
+          else pinError.emit("Please, enter a valid pin.")
+        }      
+      ),
+      div(cls("w3-border-white w3-margin w3-red"), child.text <-- pinError.events),
       div(cls("w3-bar w3-margin-top w3-center"),
         button(cls("w3-button w3-round-xxlarge w3-light-gray w3-text-indigo"), "Login").amend {
           onClick --> { _ =>
