31f4beca931b1027b60f5376ffabd559eaf7fc4b
objektwerks <objektwerks@runbox.com>
2022-01-14 03:04:37 -0500

    replaced form with div in login view

--- src/main/scala/objektwerks/LoginView.scala
+++ src/main/scala/objektwerks/LoginView.scala
@@ -6,7 +6,7 @@ import org.scalajs.dom.console.log
 
 object LoginView:
   def apply(emailAddress: Var[String], pin: Var[String]): HtmlElement =
-    form(cls("w3-container"),
+    div(cls("w3-container"),
       h5(cls("w3-light-grey w3-text-indigo"), "Login"),
       label(cls("w3-left-align w3-text-indigo"), "Email Address"),
       input(
