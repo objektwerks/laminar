fcfb902b1a1ff1e51d1af446c77368c4a00b6a03
objektwerks <objektwerks@runbox.com>
2022-01-14 14:32:48 -0500

    reformatted button code for readability

--- src/main/scala/objektwerks/LoginView.scala
+++ src/main/scala/objektwerks/LoginView.scala
@@ -46,7 +46,9 @@ object LoginView:
       div(cls("w3-bar w3-margin-top w3-center"),
         button(cls("w3-button w3-round-xxlarge w3-light-gray w3-text-indigo"),
           "Login",
-          disabled <-- emailAddress.signal.map(_.isEmailAddress()).combineWith(pin.signal.map(_.isPin())).map( (a, b) => !(a && b) )
+          disabled <-- emailAddress.signal.map(_.isEmailAddress())
+                       .combineWith(pin.signal.map(_.isPin()))
+                       .map( (a, b) => !(a && b) )
         ).amend {
           onClick --> { _ =>
             log(s"email address: ${emailAddress.now()} pin: ${pin.now()}")
