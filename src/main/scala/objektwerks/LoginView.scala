eda2cc47adb263e9cdafcf4ca8e7afe60ef66bcb
objektwerks <objektwerks@runbox.com>
2022-01-14 13:41:01 -0500

    edited build.sbt

--- src/main/scala/objektwerks/LoginView.scala
+++ src/main/scala/objektwerks/LoginView.scala
@@ -44,7 +44,11 @@ object LoginView:
       ),
       div(cls("w3-border-white w3-text-red"), child.text <-- pinError.events),
       div(cls("w3-bar w3-margin-top w3-center"),
-        button(cls("w3-button w3-round-xxlarge w3-light-gray w3-text-indigo"), "Login").amend {
+        button(cls("w3-button w3-round-xxlarge w3-light-gray w3-text-indigo"),
+          "Login",
+          disabled <-- emailAddress.signal.map(!_.isEmailAddress())
+          // disabled <-- pin.signal.map(!_.isPin())
+        ).amend {
           onClick --> { _ =>
             log(s"email address: ${emailAddress.now()} pin: ${pin.now()}")
             if emailAddress.now().isEmailAddress() && pin.now().isPin() then
