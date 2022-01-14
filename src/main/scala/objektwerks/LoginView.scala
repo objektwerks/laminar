4c507ca112c5b34b2272ae264da78ac25f921317
objektwerks <objektwerks@runbox.com>
2022-01-14 03:26:59 -0500

    refactored validator methods

--- src/main/scala/objektwerks/LoginView.scala
+++ src/main/scala/objektwerks/LoginView.scala
@@ -4,6 +4,8 @@ import com.raquo.laminar.api.L.*
 
 import org.scalajs.dom.console.log
 
+import Validator.*
+
 object LoginView:
   def apply(emailAddress: Var[String], pin: Var[String]): HtmlElement =
     div(cls("w3-container"),
@@ -33,7 +35,9 @@ object LoginView:
         button(cls("w3-button w3-round-xxlarge w3-light-gray w3-text-indigo"), "Login").amend {
           onClick --> { _ =>
             log(s"email address: ${emailAddress.now()} pin: ${pin.now()}")
-            Router.router.pushState(TasksPage)
+            if emailAddress.now().isEmailAddress() && pin.now().isPin() then
+              Router.router.pushState(TasksPage)
+            else Router.router.$currentPage
           }
         }
       )
--- src/main/scala/objektwerks/Validator.scala
+++ src/main/scala/objektwerks/Validator.scala
@@ -2,5 +2,5 @@ package objektwerks
 
 object Validator:
     extension (value: String)
-      def isEmailAddressValid: Boolean = value.nonEmpty && value.length >= 3 && value.contains("@")
-      def isPinValid: Boolean = value.length == 6
+      def isEmailAddress: Boolean = value.nonEmpty && value.length >= 3 && value.contains("@")
+      def isPin: Boolean = value.length == 6
