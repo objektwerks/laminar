15feb5b56afc2b392ad56c6eb35402203dc5bda7
objektwerks <objektwerks@runbox.com>
2022-01-17 07:39:28 -0500

    added register page

--- src/main/scala/objektwerks/Page.scala
+++ src/main/scala/objektwerks/Page.scala
@@ -5,5 +5,6 @@ import com.raquo.laminar.api.L.*
 sealed trait Page:
   val title = "Tasks"
 case object IndexPage extends Page
+case object RegisterPage extends Page
 case object LoginPage extends Page
 case object TasksPage extends Page
