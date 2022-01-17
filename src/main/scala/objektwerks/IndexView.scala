9c71815e63bd207638dbd34a3f6c25cf1d571461
objektwerks <objektwerks@runbox.com>
2022-01-17 07:32:13 -0500

    added register button to index view

--- src/main/scala/objektwerks/IndexView.scala
+++ src/main/scala/objektwerks/IndexView.scala
@@ -7,13 +7,18 @@ import org.scalajs.dom.console.log
 object IndexView:
     def apply(): HtmlElement =
       div(
-        div(cls("w3-bar w3-text-indigo"),
+        div(cls("w3-bar"),
           button(cls("w3-button w3-round-xxlarge w3-light-grey w3-text-indigo"), "Login").amend {
             onClick --> { _ =>
               log("Login onClick")
               Router.router.pushState(LoginPage)
             }
-          }
+          },
+          button(cls("w3-button w3-round-xxlarge w3-light-grey w3-text-indigo w3-right"), "Register").amend {
+            onClick --> { _ =>
+              log("Register onClick")
+            }
+          }          
         ),
         div(
           ul(cls("w3-ul"),
